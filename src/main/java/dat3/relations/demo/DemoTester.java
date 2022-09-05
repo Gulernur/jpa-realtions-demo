a6f08df569f5235db369abf0cb9058c964b021af
gulernurdonmez <gule0153@stud.kea.dk>
2022-09-05 17:53:44 +0200

    added test data

--- src/main/java/dat3/relations/demo/DemoTester.java
+++ src/main/java/dat3/relations/demo/DemoTester.java
@@ -4,6 +4,7 @@ import dat3.relations.demo.entity.Address;
 import dat3.relations.demo.entity.Person;
 import dat3.relations.demo.repositories.AddressRepository;
 import dat3.relations.demo.repositories.PersonRepository;
+import dat3.relations.demo.service.AddressService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 
@@ -12,10 +13,12 @@ public class DemoTester implements CommandLineRunner {
 
   PersonRepository personRepository;
   AddressRepository addressRepository;
+  AddressService addressService;
 
-  public DemoTester(PersonRepository personRepository, AddressRepository addressRepository) {
+  public DemoTester(PersonRepository personRepository, AddressRepository addressRepository, AddressService addressService) {
     this.personRepository = personRepository;
     this.addressRepository = addressRepository;
+    this.addressService = addressService;
   }
 
   @Override
@@ -25,15 +28,22 @@ public class DemoTester implements CommandLineRunner {
     //This is where we will add all the demo code
     Person p1 = new Person("Peter", "pe@a.dk", "hemlig");
     Person p2 = new Person("Janne", "ja@a.dk", "super-hemlig");
+    Person p3 = new Person("aaa", "hdde@a.dk", "123");
 
     Address a1 = new Address("Lyngbyvej 22", "Lyngby");
     a1.addPerson(p1);
     a1.addPerson(p2);
+    a1.addPerson(p3);
 
     addressRepository.save(a1);
 
-    System.out.println("Hit Enter to stop program");
+    System.out.println("Hit Enter to fetch an adress");
     System.in.read();
+
+    Address address = addressRepository.findById(a1.getId()).get();
+    System.out.println(address.getCity());
+
+    addressService.getAddressTrans(a1.getId(),true);
   }
 
 }
